07553037a2f43fb80eafd4299b7291edf9ca5991
qa6974010 <qa@artoonsolutions.com>
2026-03-30 12:10:33 +0530

    NE-MNCTIXWFWGM[in-progress]: to see commits by dk

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-console.log("welcome to the github");
-console.log("welcome to the github");
-console.log("welcome to the github");
-console.log("welcome to the github");
-console.log("welcome to the github");
-console.log("welcome to the github");
+console.log("First Line of code");
+console.log("Second Line of code");
+console.log("Third Line of code");
+setTimeout(() => {
+    console.log("Interval of 5 seconds completed");
+}, 5000);
+console.log("Fourth Line of code");
+console.log("Fifth Line of code");
+console.log("Sixth Line of code");
